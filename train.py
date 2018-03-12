12311888e585900ed9194e3422709e0bf1244b75
bdvllrs <benjamin.devillers@supelec.fr>
2018-03-12 21:59:15 +0100

    add config in default file

--- train.py
+++ train.py
@@ -1,7 +1,30 @@
 from utils import ScribAPI, ScribDataset
 
-scribAPI = ScribAPI('./config/local.json')
+config_file = './config/local.json'
 
-preferences = scribAPI.get_preferences('onmt')
-# result = scribAPI.add_preference_number_of_view(8)
-print(len(preferences))
+scribAPI = ScribAPI(config_file)
+
+"""
+Get preferences from the database.
+
+Dict format is a list of preferences dict. There is the format of the dicts:
+* id
+* score:
+  * -1 none of the choices is good
+  * 0 both choices are good
+  * 1 left action is better
+  * 2 right action is better
+* treated: number of time you requested this preference
+* action_right dict:
+  * id
+  * content: summary content
+  * Article dict
+    * fullText: article
+* action_left dict:
+  * id
+  * content: summary content
+  * Article dict
+    * fullText: article
+"""
+preferences = scribAPI.get_preferences(scribAPI.config['model']['name'])
+print(preferences)
